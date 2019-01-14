b22513c040b16f0e96c490852ee72c3b863cc46b
nguyenthanhson <son.thanh.97@gmail.com>
2019-01-14 11:44:58 +0700

    Fix Secret key base error on production

--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -1,5 +1,5 @@
 class JsonWebToken
-  HMAC_SECRET = Rails.application.credentials.secret_key_base
+  HMAC_SECRET = Rails.application.credentials.secret_key_base || ENV["SECRET_KEY_BASE"]
 
   def self.encode(payload, exp = 24.hours.from_now)
     payload[:exp] = exp.to_i
