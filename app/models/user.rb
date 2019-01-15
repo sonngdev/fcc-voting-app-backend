0dcc2b2b795b11802db660aeda25f6dcd966118a
nguyenthanhson <son.thanh.97@gmail.com>
2019-01-15 11:27:26 +0700

    Ensure uniqueness of username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :polls, foreign_key: :created_by
-  validates_presence_of :username, :password_digest
+  validates :username, uniqueness: true, presence: true
+  validates_presence_of :password_digest
 end
