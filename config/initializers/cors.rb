e82c629a660170e00eff6cd50e74264047b22741
nguyenthanhson <son.thanh.97@gmail.com>
2019-01-20 00:57:25 +0700

    Enable CORS for github pages

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,6 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'localhost:3000'
+    origins 'https://nguyen-thanhson.github.io'
 
     resource '*',
       headers: :any,
